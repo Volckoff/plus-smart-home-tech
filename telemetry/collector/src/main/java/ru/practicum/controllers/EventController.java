54a8903a9e8ca76bed64724ef9da76ceed06b1be
Volckoff <Voronenok19@yandex.ru>
2025-09-18 19:38:04 +0300

    Добавлено логирование в контроллер и локальный запуск для дебага

--- telemetry/collector/src/main/java/ru/practicum/controllers/EventController.java
+++ telemetry/collector/src/main/java/ru/practicum/controllers/EventController.java
@@ -2,6 +2,7 @@ package ru.practicum.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import ru.practicum.event.sensor.SensorEvent;
 import ru.practicum.sevices.EventService;
 
 @RestController
+@Slf4j
 @RequestMapping("/events")
 @RequiredArgsConstructor
 public class EventController {
@@ -18,11 +20,15 @@ public class EventController {
 
     @PostMapping("/sensors")
     public void collectSensorEvent(@Valid @RequestBody SensorEvent event) {
+        log.info("Получено событие датчика: {}", event);
         eventService.publishSensorEvent(event);
+        log.info("Событие датчика обработано: {}", event);
     }
 
     @PostMapping("/hubs")
     public void collectHubEvent(@Valid @RequestBody HubEvent event) {
+        log.info("Получено событие датчика: {}", event);
         eventService.publishHubEvent(event);
+        log.info("Событие датчика обработано: {}", event);
     }
 }
